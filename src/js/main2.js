1f0b244cc47971913b5d995cfed91a0de4e73110
vlad-zhadan <zhadan.vld@gmail.com>
2023-12-11 21:36:42 +0200

    add vector map

--- src/js/main2.js
+++ src/js/main2.js
@@ -1,44 +1,54 @@
 
+const key = 'Wllbp5hPJZa4ZoFvKPiW';
+const styleJson = `https://api.maptiler.com/maps/streets-v2/style.json?key=${key}`;
+
+const attribution = new ol.control.Attribution({
+    collapsible: false,
+});
+
+const bounds = ol.proj.transformExtent(
+    [34.77, 50.88, 34.83, 50.94], // [minX, minY, maxX, maxY] in longitude and latitude
+    'EPSG:4326', // Source projection (longitude/latitude)
+    'EPSG:3857'  // Target projection (web mercator)
+);
 
 
-
-const mapTilerKey = 'Wllbp5hPJZa4ZoFvKPiW';
-const mapTilerUrl = `https://api.maptiler.com/maps/streets/{z}/{x}/{y}.png?key=${mapTilerKey}&scaleFactor=@2x`;
 
 const map = new ol.Map({
     target: 'map',
-    layers: [
-         new ol.layer.VectorTile({
-            source: new ol.source.VectorTile({
-                format: new ol.format.MVT(),
-                url: `https://api.maptiler.com/tiles/v3/{z}/{x}/{y}.pbf?key=${mapTilerKey}`,
-                tileSize: 512, // MapTiler's vector tiles are 512x512
-                maxZoom: 14 // Adjust max zoom as needed
-            })
-        }),
-        new ol.layer.Tile({
-            source: new ol.source.XYZ({
-                url: mapTilerUrl
-            })
-        })
-       
-    ],
-    
-        
-    
+    controls: ol.control.defaults.defaults({ attribution: false }).extend([attribution]),
     view: new ol.View({
-        center: ol.proj.fromLonLat([34.7996656, 50.8997286]), // Replace with your coordinates
-        zoom: 20
-        
+        center: ol.proj.fromLonLat([34.7996656, 50.8997286]),
+        smoothResolutionConstraint: true, // Enabling smoother transitions between zoom levels
+        constrainResolution: false, // Allows intermediate zoom levels
+        zoom: 16,
+        extent: bounds // Apply the bounds here
     })
-});
+   
+
+})
+
+olms.apply(map, styleJson);
 
 const vectorSource = new ol.source.Vector({wrapX: false});
 
 // Create a vector layer using the vector source
 const vectorLayer = new ol.layer.Vector({
-    source: vectorSource
+    source: vectorSource,
+    style: new ol.style.Style({
+        fill: new ol.style.Fill({
+            color: 'rgba(255, 255, 255, 0.6)', // Change color and opacity as needed
+        }),
+        stroke: new ol.style.Stroke({
+            color: '#ffcc33',
+            width: 2
+        })
+    })
+
 });
+
+console.log(map); // Check if the map is initialized
+console.log(vectorLayer); 
 
 // Add the vector layer to the map
 map.addLayer(vectorLayer);
@@ -56,4 +66,6 @@ map.addInteraction(draw);
 draw.on('drawend', function(event) {
     // Do something with the drawn polygon if needed
     // event.feature is the drawn polygon
+    console.log(event.feature); 
 });
+
